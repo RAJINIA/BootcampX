cc97ec150f82ee39ec1afb99e10b71335f5ff635
RAJINIA <rajini1608@gmail.com>
2021-11-25 00:59:32 -0600

    SQL Injection Parameterized Query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,6 +6,9 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
